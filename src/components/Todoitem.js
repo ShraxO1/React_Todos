f4e54b36a1c406cdef1b22596bbe1097ef5a296a
ShraxO1 <shraytyagi1408@gmail.com>
2020-10-29 15:58:54 +0530

    first commit

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Todoitem extends Component{
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+        }
+    }
     render() {
+        const { id,title } = this.props.todo;
         return (
-            <div>
-                <p>{this.props.todo.title}</p>
+            <div style={this.getStyle()}>
+                <input type="checkbox" onChange={this.props.markComplete.bind
+                (this, id)}/> {' '}
+                <p>{title}</p>
             </div>
         );
     }
 }
+
+Todoitem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
 
 export default Todoitem;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Todoitem from "./Todoitem";
+import PropTypes from 'prop-types';
 
 class Todos extends Component{
     render() {
         return this.props.todos.map((todo) => (
-            <Todoitem todo={todo}/>
+            <Todoitem key={todo.id} markComplete = {this.props.markComplete} todo={todo}/>
         ));
     }
+}
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
 }
 
 export default Todos;
